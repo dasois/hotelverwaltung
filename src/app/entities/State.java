f9f97f77bf797b50e670c824722b234a3ffff8d4
Tobias Mayer <Mayer25@hm.edu>
2014-06-25 19:22:17 +0200

    reworked

--- src/app/entities/State.java
+++
@@ -1,3 +0,0 @@
-package app.entities;
-
-public enum State {Init,Kunde,Zimmer,Dienstleistung}
--- src/gui/IModel.java
+++
@@ -1,7 +0,0 @@
-package gui;
-
-import app.entities.State;
-
-public interface IModel {
-	public boolean checkCorrespondingState(State s);
-}
